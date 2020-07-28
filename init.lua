1a5fa915c26ce4497472838fcc3bae533f8d32d0
Emojigit <55009343+Emojigit@users.noreply.github.com>
2020-07-28 17:25:18 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,38 @@
+technic_sawmill = {}
+technic_sawmill.mp = minetest.get_modpath("technic_sawmill")
+dofile(technic_sawmill.mp.."/recipe.lua")
+
+function technic_sawmill.register_sawmill(data)
+	data.typename = "sawmilling"
+	data.machine_name = "sawmill"
+	data.machine_desc = "%s Sawmill"
+	technic.register_base_machine(data)
+end
+
+
+minetest.register_alias("sawmill", "technic:lv_sawmill")
+minetest.register_craft({
+	output = 'technic:lv_sawmill',
+	recipe = {
+		{'technic:stainless_steel_ingot', 'technic:diamond_drill_head', 'technic:stainless_steel_ingot'},
+		{'technic:fine_copper_wire',      'technic:machine_casing',     'technic:motor'},
+		{'technic:carbon_steel_ingot',    'technic:lv_cable',           'technic:carbon_steel_ingot'},
+	},
+})
+
+technic_sawmill.register_sawmill({tier="LV", demand={200}, speed=1, 
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-3/32, 14/32, -1/32, 3/32, 15/32, 1/32},
+			{-6/32, 13/32, -1/32, 6/32, 14/32, 1/32},
+			{-8/32, 12/32, -1/32, 8/32, 13/32, 1/32},
+			{-9/32, 11/32, -1/32, 9/32, 12/32, 1/32},
+			{-10/32, 10/32, -1/32, 10/32, 11/32, 1/32},
+			{-11/32, 9/32, -1/32, 11/32, 10/32, 1/32},
+			{-12/32, 7/32, -1/32, 12/32, 9/32, 1/32},
+			{-1/2, -1/2, -1/2, 1/2, 7/32, 1/2},
+		},
+	}, 
+	tiles = {nil,nil,nil,nil,"_back",nil}})
+
